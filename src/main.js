123e2774922312b0d77684e8f2d5cdd6435efafd
Aitaro Chaya <aitaro.chaya@gmail.com>
2020-05-18 12:23:46 +0900

    default settings

--- src/main.js
+++ src/main.js
@@ -4,6 +4,34 @@ import store from "./store";
 
 Vue.config.productionTip = false;
 
+// init communication
+
+// wh.im本体との通信を開始
+window.parent.postMessage("connect", document.referrer);
+
+// wh.imから room / users情報が送られてきたら登録
+window.addEventListener(
+  "message",
+  event => {
+    console.log("get event: " + event);
+    if (event.data.room) {
+      console.log("room: " + event.data.room);
+      this.$store.commit("setRoom", event.data.room);
+    } else if (event.data.users) {
+      console.log("user: " + event.data.users);
+      this.$store.commit("setUsers", event.data.users);
+      this.$store.dispatch("setUserId", this.$route.query.userId);
+      if (this.$route.query.displayUserId) {
+        this.$store.dispatch(
+          "setDisplayUserId",
+          this.$route.query.displayUserId
+        );
+      }
+    }
+  },
+  false
+);
+
 new Vue({
   store,
   render: h => h(App)
--- src/store/index.js
+++ src/store/index.js
@@ -4,8 +4,31 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    room: {}, // room information
+    users: [], // information of users in the room
+    user: {}, // information of user who play in this window
+    appState: {}
+  },
+  mutations: {
+    setRoom(state, room) {
+      state.room = room;
+      state.appState = room.appState;
+    },
+    setUsers(state, users) {
+      console.log(users);
+      state.users = users;
+    },
+    setUser(state, userId) {
+      const user = state.users.find(user => user.id === userId);
+      state.user = user;
+    }
+  },
+  actions: {
+    // put original actions here
+  },
+  getters: {
+    // put original getters here
+  },
   modules: {}
 });
