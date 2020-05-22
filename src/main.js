2e5ab69ed85651fb25a4c7dddd5a9cd945997f89
Aitaro Chaya <aitaro.chaya@gmail.com>
2020-05-22 21:03:08 +0900

    リファクタ

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import Vue from "vue";
 import App from "./App.vue";
-import store from "./store";
+import Vuex from "vuex";
 import whimClientVue from "whim-client-vue";
+
+Vue.use(Vuex);
+
+const store = new Vuex.Store();
 
 Vue.config.productionTip = false;
 Vue.use(whimClientVue, { store });
--- src/store/index.js
+++ src/store/index.js
@@ -4,53 +4,7 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 const store = new Vuex.Store({
-  state: {
-    room: {}, // room information
-    users: [], // information of users in the room
-    accessUserId: null, // information of user who play in this window
-    appState: {}
-  },
-  mutations: {
-    setRoom(state, room) {
-      state.room = room;
-    },
-    setUsers(state, users) {
-      state.users = users;
-    },
-    setAccessUserId(state, userId) {
-      state.accessUserId = userId;
-    },
-    setAppState(state, appState) {
-      console.log(appState);
-      state.appState = appState;
-    }
-  },
-  actions: {
-    appState({ state }, obj) {
-      let appState = state.appState;
-      Object.keys(obj).forEach(key => {
-        appState[key] = obj[key];
-      });
-      postAppState(appState);
-    },
-    resetAppState() {
-      postAppState({});
-    }
-    // put original actions here
-  },
-  getters: {
-    accessUser: state => {
-      return state.users.find(user => user.id === state.accessUserId);
-    }
-    // put original getters here
-  },
   modules: {}
 });
-
-// appStateの更新、clinet & server
-const postAppState = appState => {
-  store.commit("setAppState", appState);
-  window.parent.postMessage({ appState }, document.referrer);
-};
 
 export default store;
