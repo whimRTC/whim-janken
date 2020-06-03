8d0ef6a69c03fa331c90108f6e9ee433d180996f
Aitaro Chaya <aitaro.chaya@gmail.com>
2020-06-03 19:11:26 +0900

    開発環境、stg環境でも使えるように

--- src/main.js
+++ src/main.js
@@ -1,17 +1,24 @@
 import Vue from "vue";
 import App from "./App.vue";
-import Vuex from "vuex";
 import whimClientVue from "whim-client-vue";
 import "whim-client-vue/dist/whim-client-vue.css";
 
-Vue.use(Vuex);
-const store = new Vuex.Store({});
-
 Vue.config.productionTip = false;
-// TODO environment
-Vue.use(whimClientVue, { store });
+
+// 通常はこれだけでよい
+// Vue.use(whimClientVue);
+
+// wh.imの開発
+let targetOrigin;
+if (window.location.search === "?env=dev") {
+  targetOrigin = "https://localhost:3000";
+} else if (window.location.search === "?env=stg") {
+  targetOrigin = "https://stg.wh.im";
+} else {
+  targetOrigin = "https://wh.im";
+}
+Vue.use(whimClientVue, { targetOrigin });
 
 new Vue({
-  store,
   render: h => h(App)
 }).$mount("#app");
