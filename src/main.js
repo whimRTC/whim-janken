09152ec97800894ac1ecd499bfe17a4ac90c6f76
Aitaro Chaya <aitaro.chaya@gmail.com>
2020-07-15 15:21:07 +0900

    仕様変更にともない追記

--- src/main.js
+++ src/main.js
@@ -5,14 +5,21 @@ import "whim-client-vue/dist/whim-client-vue.css";
 
 Vue.config.productionTip = false;
 
+// settings for debug
+// let recaptchaScript = document.createElement("script");
+// recaptchaScript.setAttribute("src", "http://localhost:8098");
+// document.head.appendChild(recaptchaScript);
+
 // 通常はこれだけでよい
 // Vue.use(whimClientVue);
 
 // wh.imの開発時に用いるの設定
 let targetOrigin;
-if (window.location.search === "?env=dev") {
-  targetOrigin = "https://localhost:3000";
-} else if (window.location.search === "?env=stg") {
+
+const params = new URLSearchParams(window.location.search);
+if (params.get("env") === "dev") {
+  targetOrigin = "http://localhost:3000";
+} else if (params.get("env") === "stg") {
   targetOrigin = "https://stg.wh.im";
 } else {
   targetOrigin = "https://wh.im";
